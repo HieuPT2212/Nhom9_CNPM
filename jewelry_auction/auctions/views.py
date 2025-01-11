967af16e7db9e2b80762b10b29bee971bd4c7cbd
HieuPT2212 <mainpth2212@gmail.com>
2025-01-11 17:44:04 +0700

    fix

--- jewelry_auction/auctions/views.py
+++ jewelry_auction/auctions/views.py
@@ -1,61 +1,109 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Bid
-from auctions.models import Auction
-from .forms import BidForm
-from django.contrib.auth.decorators import login_required
-from django.db import transaction
+from .models import Auction
+from .forms import AuctionForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponseForbidden
-from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from notifications.models import Notification
+from django.db.models import Q
 
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Bid
-from auctions.models import Auction
-from .forms import BidForm
-from django.contrib.auth.decorators import login_required
-from django.db import transaction
-from django.http import HttpResponseForbidden
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from notifications.models import Notification
+def is_manager(user):
+    return user.role == 'MANAGER'
 
 @login_required
-def place_bid(request, auction_id):
-    auction = get_object_or_404(Auction, pk=auction_id)
-
-    if auction.status != 'OPEN':
-        return HttpResponseForbidden("This auction is not open for bidding.")
-
+@user_passes_test(is_manager)
+def auction_create(request):
     if request.method == 'POST':
-        form = BidForm(request.POST)
-        # Gán user và auction vào form.instance trước khi gọi is_valid()
-        form.instance.user = request.user
-        form.instance.auction = auction
+        form = AuctionForm(request.POST)
         if form.is_valid():
-            try:
-                with transaction.atomic():
-                    bid = form.save()
-                    return redirect('auctions:auction-detail', pk=auction_id)
-            except ValidationError as e:
-                form.add_error(None, e)
+            auction = form.save(commit=False)
+            auction.manager = request.user
+            auction.save()
+            return redirect('auctions:auction-list')
     else:
-        form = BidForm()
-
-    return render(request, 'bids/place_bid.html', {'form': form, 'auction': auction})
+        form = AuctionForm()
+    return render(request, 'auctions/auction_create.html', {'form': form})
 
 @login_required
-def user_bids(request):
-    bids = Bid.objects.filter(user=request.user).order_by('-timestamp')
+def auction_list(request):
+    auction_list = Auction.objects.all()
+
+    # Lọc theo trạng thái nếu user là MANAGER
+    if request.user.is_authenticated and request.user.role == 'MANAGER':
+        status = request.GET.get('status')
+        if status:
+            auction_list = auction_list.filter(status=status)
+    else:
+        auction_list = auction_list.filter(status__in=['OPEN', 'CLOSED'])
     
+    # Search
+    search_query = request.GET.get('search')
+    if search_query:
+        auction_list = auction_list.filter(
+            Q(jewelry__name__icontains=search_query) |
+            Q(jewelry__owner__username__icontains=search_query) |
+            Q(manager__username__icontains=search_query) |
+            Q(staff__username__icontains=search_query)
+        )
+
+    # Sort
+    sort_by = request.GET.get('sort')
+    if sort_by:
+        auction_list = auction_list.order_by(sort_by)
+
     # Pagination
-    paginator = Paginator(bids, 10)  # Show 10 bids per page
+    paginator = Paginator(auction_list, 5)  # Show 5 auctions per page
     page = request.GET.get('page')
     try:
-        bids_page = paginator.page(page)
+        auctions = paginator.page(page)
     except PageNotAnInteger:
-        bids_page = paginator.page(1)
+        auctions = paginator.page(1)
     except EmptyPage:
-        bids_page = paginator.page(paginator.num_pages)
+        auctions = paginator.page(paginator.num_pages)
     
-    return render(request, 'bids/user_bids.html', {'bids': bids_page})
+    return render(request, 'auctions/auction_list.html', {'auctions': auctions})
+
+def auction_detail(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    return render(request, 'auctions/auction_detail.html', {'auction': auction})
+
+@login_required
+@user_passes_test(is_manager)
+def auction_update(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    if request.user != auction.manager:
+        return HttpResponseForbidden("You don't have permission to edit this auction.")
+    if request.method == 'POST':
+        form = AuctionForm(request.POST, instance=auction)
+        if form.is_valid():
+            form.save()
+            return redirect('auctions:auction-detail', pk=auction.pk)
+    else:
+        form = AuctionForm(instance=auction)
+    return render(request, 'auctions/auction_update.html', {'form': form})
+
+@login_required
+@user_passes_test(is_manager)
+def auction_delete(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    if request.user != auction.manager:
+        return HttpResponseForbidden("You don't have permission to delete this auction.")
+    if request.method == 'POST':
+        auction.delete()
+        return redirect('auctions:auction-list')
+    return render(request, 'auctions/auction_delete.html', {'auction': auction})
+
+@login_required
+@user_passes_test(is_manager)
+def auction_cancel(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    if request.user != auction.manager:
+        return HttpResponseForbidden("You don't have permission to cancel this auction.")
+    if request.method == 'POST':
+        if auction.status == 'CREATED':
+            auction.status = 'CANCELED'
+            auction.save()
+            return redirect('auctions:auction-list')
+        else:
+            # Xử lý trường hợp auction không ở trạng thái CREATED
+            return redirect('auctions:auction-detail', pk=auction.pk)
+    return render(request, 'auctions/auction_cancel.html', {'auction': auction})
