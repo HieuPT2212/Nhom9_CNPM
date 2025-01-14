d8f28b5a0ab82811acfd3b59df21e9886e30e48e
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-01-14 15:28:52 +0700

    update tests.py core

--- jewelry_auction/core/tests.py
+++ jewelry_auction/core/tests.py
@@ -2,16 +2,9 @@ from django.test import TestCase, Client
 from blog.models import Blog
 from jewelry.models import Jewelry
 from notifications.models import Notification
-from users.models import User
-# Create your tests here.
 class ViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.blog = Blog.objects.create(blog_id =1,title='Test Blog', content='This is a test blog.', author=self.user)
-        self.jewelry = Jewelry.objects.create(jewelry_id=1, name='Test Jewelry', description='This is a test jewelry.', owner=self.user, initial_price = 1000)
-        self.notification = Notification.objects.create(user=self.user, message='This is a test notification.')
-
         self.home_url = 'core/index.html'
         self.about_url = 'core/about.html'
         self.policy_url = 'core/policy.html'
